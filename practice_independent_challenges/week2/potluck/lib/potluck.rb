d6be09c15519e3d7d00c47f2f37b9e76e34738be
Noah Durbin <noahdurbin@gmail.com>
2024-03-27 17:29:17 -0600

    added a test for a get all from category

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -2,7 +2,14 @@ class Potluck
 
     attr_reader :date
 
+    attr_accessor :dishes
+
     def initialize(date)
         @date = date
+        @dishes = []
+    end
+
+    def add_dish(dish)
+        @dishes << dish
     end
 end
--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -1,4 +1,6 @@
 require './lib/potluck.rb'
+require './lib/dish.rb'
+
 
 RSpec.describe Potluck do
     before :each do
@@ -11,5 +13,36 @@ RSpec.describe Potluck do
 
     it "contains a potluck date" do
         expect(@potluck.date).to eq("7-13-18")
+    end
+
+    it "starts with an empty array of dishes" do
+        expect(@potluck.dishes).to eq([])
+    end
+
+    it "can add dishes to the dishes array" do
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        @potluck.add_dish(couscous_salad)
+        @potluck.add_dish(cocktail_meatballs)
+
+        expect(@potluck.dishes).to eq([couscous_salad, cocktail_meatballs])
+        expect(@potluck.dishes.length).to eq(2)
+    end
+
+    it "can get all dishes from a category" do
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        summer_pizza = Dish.new("Summer Pizza", :appetizer)
+        roast_pork = Dish.new("Roast Pork", :entre)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        candy_salad = Dish.new("Candy Salad", :dessert)
+        @potluck.add_dish(couscous_salad)
+        @potluck.add_dish(summer_pizza)
+        @potluck.add_dish(roast_pork)
+        @potluck.add_dish(cocktail_meatballs)
+        @potluck.add_dish(candy_salad)
+        
+        expect(@potluck.get_all_from_category(:appetizer)).to eq([couscous_salad, summer_pizza])
+        expect(@potluck.get_all_from_category(:entre)).to eq([roast_pork, cocktail_meatballs])
+        expect(@potluck.get_all_from_category(:dessert)).to eq([candy_salad])
     end
 end
